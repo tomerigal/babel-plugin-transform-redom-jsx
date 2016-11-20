ec0911307f7cb47bda7e4c50c572274bfd5de8a0
barak igal <baraki@wix.com>
2016-11-20 02:51:11 +0200

    Component
    added support for components

--- test/index.js
+++ test/index.js
@@ -3,21 +3,20 @@ var redomT = require("../src/index");
 var expect = require("chai").expect;
 var path = require("path");
 var fs = require("fs");
-function trim(str) {return str.replace(/^\s+|\s+$/, "");}
-function removeSpaces(str){return str.replace(/\s/gm, "");}
+
+function trim(str) { return str.replace(/^\s+|\s+$/, ""); }
+function removeSpaces(str) { return str.replace(/\s/gm, ""); }
+
 describe("transform redom jsx", () => {
     const fixturesDir = path.join(__dirname, "fixtures");
     fs.readdirSync(fixturesDir).map((caseName) => {
         it(`should ${caseName.split("-").join(" ")}`, () => {
             const fixtureDir = path.join(fixturesDir, caseName);
             const actual = babel.transformFileSync(
-                path.join(fixtureDir, "actual.js"),
-                {
-                    plugins: [ 'syntax-jsx', redomT]
-                }
+                path.join(fixtureDir, "actual.js"), { plugins: [redomT] }
             );
             const expected = fs.readFileSync(path.join(fixtureDir, "expected.js")).toString();
             expect(removeSpaces(actual.code)).to.equal(removeSpaces(expected));
         });
-  });
+    });
 });
